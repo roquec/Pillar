323c25065e0f3ad04ffd02e1d2ef5e7b18112986
RoqueC <roquecarrizof@gmail.com>
2022-03-30 00:53:05 +0200

    Extend project structure

---
+++ tests/unit/Pillar.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace Pillar.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ tests/unit/Tracing/Pillar.Tracing.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace Pillar.Tracing.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
